77ed57b477b58e55edc8621bec987f1fa657d617
William Phelps <williamphelps13@gmail.com>
2021-07-11 17:46:02 -0600

    Add a test to test if it is a function and an instance

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,25 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+import userData from '../src/data/users';
 
 describe('User Repository', () => {
-  it('should be a function', function () {
+  let userRepository
+
+  beforeEach(() => {
+    userRepository = new UserRepository(userData);
+  });
+
+  it('should be a function', () => {
     expect(UserRepository).to.be.a('function');
   });
+
+  it('should be an instance of a UserRepository', () => {
+    expect(userRepository).to.be.an.instanceOf(UserRepository);
+  });
+
+  it('should be able to hold user data', () => {
+    
+    expect().to.equal();
+  });
+  
 });

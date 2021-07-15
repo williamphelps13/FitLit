72f3f5becabc50b8f62ea2fc7012f8617003fcb8
Justin Anthony <justincanthony@gmail.com>
2021-07-15 15:19:14 -0600

    Create method for calculating avg water consumed

--- src/HydrationRepo.js
+++ src/HydrationRepo.js
@@ -2,11 +2,18 @@ class HydrationRepo {
   constructor(data) {
     this.data = data;
   }
-}
 
-/*
+  getAvgOuncesAllTime(id) {
+    const userDays = this.data.filter((user) => user.userID === id);
+    const totalUserOunces = userDays.reduce((totalOunces, user) => {
+      return (totalOunces += user.numOunces);
+    }, 0);
+    return totalUserOunces / userDays.length;
+  }
+
+  /*
 Data
-- For a user (identified by their userID - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
+// - For a user (identified by their userID - this is the same for all methods requiring a specific user’s data), the average fluid ounces consumed per day for all time
 
 - For a user, how many fluid ounces they consumed for a specific day (identified by a date)
 
@@ -18,4 +25,6 @@ For your user (or any user you choose), add:
 - A display to show how much water they have consumed today (these displays are often called “widgets” in the FE tech world)
 
 - A display to show much water they have consumed each day over the course of the latest week*/
+}
+
 export default HydrationRepo;

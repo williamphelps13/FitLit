b17e27b2709bc34c65728494ef68788cc56ac3d8
Justin Anthony <justincanthony@gmail.com>
2021-07-16 14:59:02 -0600

    No change

--- src/data/user-test-data.js
+++ src/data/user-test-data.js
@@ -1,235 +1,157 @@
 const userData = [
   {
-    "id": 1,
-    "name": "Luisa Hane",
-    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-    "email": "Diana.Hayes1@hotmail.com",
-    "strideLength": 4.3,
-    "dailyStepGoal": 10000,
-    "friends": [
-      16,
-      4,
-      8
-    ]
+    id: 1,
+    name: 'Luisa Hane',
+    address: '15195 Nakia Tunnel, Erdmanport VA 19901-1697',
+    email: 'Diana.Hayes1@hotmail.com',
+    strideLength: 4.3,
+    dailyStepGoal: 10000,
+    friends: [16, 4, 8],
   },
   {
-    "id": 2,
-    "name": "Jarvis Considine",
-    "address": "30086 Kathryn Port, Ciceroland NE 07273",
-    "email": "Dimitri.Bechtelar11@gmail.com",
-    "strideLength": 4.5,
-    "dailyStepGoal": 5000,
-    "friends": [
-      9,
-      18,
-      24,
-      19
-    ]
+    id: 2,
+    name: 'Jarvis Considine',
+    address: '30086 Kathryn Port, Ciceroland NE 07273',
+    email: 'Dimitri.Bechtelar11@gmail.com',
+    strideLength: 4.5,
+    dailyStepGoal: 5000,
+    friends: [9, 18, 24, 19],
   },
   {
-    "id": 3,
-    "name": "Herminia Witting",
-    "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
-    "email": "Elwin.Tromp@yahoo.com",
-    "strideLength": 4.4,
-    "dailyStepGoal": 5000,
-    "friends": [
-      19,
-      11,
-      42,
-      33
-    ]
+    id: 3,
+    name: 'Herminia Witting',
+    address: '85823 Bosco Fork, East Oscarstad MI 85126-5660',
+    email: 'Elwin.Tromp@yahoo.com',
+    strideLength: 4.4,
+    dailyStepGoal: 5000,
+    friends: [19, 11, 42, 33],
   },
   {
-    "id": 4,
-    "name": "Mae Connelly",
-    "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
-    "email": "Marcos_Pollich@hotmail.com",
-    "strideLength": 3.1,
-    "dailyStepGoal": 4000,
-    "friends": [
-      48,
-      7,
-      44,
-      8
-    ]
+    id: 4,
+    name: 'Mae Connelly',
+    address: '28926 Schinner Islands, Turnermouth NE 23720-3230',
+    email: 'Marcos_Pollich@hotmail.com',
+    strideLength: 3.1,
+    dailyStepGoal: 4000,
+    friends: [48, 7, 44, 8],
   },
   {
-    "id": 5,
-    "name": "Erick Schaden",
-    "address": "514 Mayert Walk, Jordaneside SC 55023-6523",
-    "email": "Vanessa_Gerhold@gmail.com",
-    "strideLength": 3.1,
-    "dailyStepGoal": 8000,
-    "friends": [
-      13,
-      44,
-      49,
-      33,
-      10
-    ]
+    id: 5,
+    name: 'Erick Schaden',
+    address: '514 Mayert Walk, Jordaneside SC 55023-6523',
+    email: 'Vanessa_Gerhold@gmail.com',
+    strideLength: 3.1,
+    dailyStepGoal: 8000,
+    friends: [13, 44, 49, 33, 10],
   },
   {
-    "id": 6,
-    "name": "Jerrold Bogisich",
-    "address": "8283 Carroll Harbor, Borerfort CT 69020-3448",
-    "email": "Mercedes_Zboncak53@yahoo.com",
-    "strideLength": 3.7,
-    "dailyStepGoal": 11000,
-    "friends": [
-      11,
-      48,
-      15
-    ]
+    id: 6,
+    name: 'Jerrold Bogisich',
+    address: '8283 Carroll Harbor, Borerfort CT 69020-3448',
+    email: 'Mercedes_Zboncak53@yahoo.com',
+    strideLength: 3.7,
+    dailyStepGoal: 11000,
+    friends: [11, 48, 15],
   },
   {
-    "id": 7,
-    "name": "Breanne Fay",
-    "address": "834 Retta Knoll, Stantonland MA 71627-4121",
-    "email": "Dashawn28@gmail.com",
-    "strideLength": 2.9,
-    "dailyStepGoal": 8000,
-    "friends": [
-      12,
-      27,
-      22,
-      30
-    ]
+    id: 7,
+    name: 'Breanne Fay',
+    address: '834 Retta Knoll, Stantonland MA 71627-4121',
+    email: 'Dashawn28@gmail.com',
+    strideLength: 2.9,
+    dailyStepGoal: 8000,
+    friends: [12, 27, 22, 30],
   },
   {
-    "id": 8,
-    "name": "Laney Abshire",
-    "address": "86416 Koch Inlet, North Kaciefurt MA 80635",
-    "email": "Janice_Nitzsche2@yahoo.com",
-    "strideLength": 2.8,
-    "dailyStepGoal": 2000,
-    "friends": [
-      11,
-      41,
-      23,
-      49
-    ]
+    id: 8,
+    name: 'Laney Abshire',
+    address: '86416 Koch Inlet, North Kaciefurt MA 80635',
+    email: 'Janice_Nitzsche2@yahoo.com',
+    strideLength: 2.8,
+    dailyStepGoal: 2000,
+    friends: [11, 41, 23, 49],
   },
   {
-    "id": 9,
-    "name": "Myron Schmitt",
-    "address": "85251 Martina Fields, West Aletha MD 00163-5315",
-    "email": "Gerard_Langosh22@hotmail.com",
-    "strideLength": 3.8,
-    "dailyStepGoal": 6000,
-    "friends": [
-      16,
-      26,
-      17
-    ]
+    id: 9,
+    name: 'Myron Schmitt',
+    address: '85251 Martina Fields, West Aletha MD 00163-5315',
+    email: 'Gerard_Langosh22@hotmail.com',
+    strideLength: 3.8,
+    dailyStepGoal: 6000,
+    friends: [16, 26, 17],
   },
   {
-    "id": 10,
-    "name": "Roslyn Bernhard",
-    "address": "22859 Sean Meadow, Lake Dorthyfort MA 89560",
-    "email": "Hillary83@gmail.com",
-    "strideLength": 4.1,
-    "dailyStepGoal": 3000,
-    "friends": [
-      40,
-      33
-    ]
+    id: 10,
+    name: 'Roslyn Bernhard',
+    address: '22859 Sean Meadow, Lake Dorthyfort MA 89560',
+    email: 'Hillary83@gmail.com',
+    strideLength: 4.1,
+    dailyStepGoal: 3000,
+    friends: [40, 33],
   },
   {
-    "id": 11,
-    "name": "Dannie Berge",
-    "address": "52042 Douglas Passage, Port Faye NC 82258",
-    "email": "Fabian_Murray92@yahoo.com",
-    "strideLength": 2.9,
-    "dailyStepGoal": 9000,
-    "friends": [
-      15,
-      19
-    ]
+    id: 11,
+    name: 'Dannie Berge',
+    address: '52042 Douglas Passage, Port Faye NC 82258',
+    email: 'Fabian_Murray92@yahoo.com',
+    strideLength: 2.9,
+    dailyStepGoal: 9000,
+    friends: [15, 19],
   },
   {
-    "id": 12,
-    "name": "Otis Kuhic",
-    "address": "853 Kulas Highway, Port Estellbury WI 96713",
-    "email": "Morton40@hotmail.com",
-    "strideLength": 3.4,
-    "dailyStepGoal": 11000,
-    "friends": [
-      28,
-      41,
-      24,
-      23
-    ]
+    id: 12,
+    name: 'Otis Kuhic',
+    address: '853 Kulas Highway, Port Estellbury WI 96713',
+    email: 'Morton40@hotmail.com',
+    strideLength: 3.4,
+    dailyStepGoal: 11000,
+    friends: [28, 41, 24, 23],
   },
   {
-    "id": 13,
-    "name": "Tom Schmeler",
-    "address": "1524 Clemmie River, Newtonbury RI 02849-3159",
-    "email": "Leopoldo.Sauer@gmail.com",
-    "strideLength": 3.2,
-    "dailyStepGoal": 4000,
-    "friends": [
-      33,
-      14,
-      3,
-      43,
-      35
-    ]
+    id: 13,
+    name: 'Tom Schmeler',
+    address: '1524 Clemmie River, Newtonbury RI 02849-3159',
+    email: 'Leopoldo.Sauer@gmail.com',
+    strideLength: 3.2,
+    dailyStepGoal: 4000,
+    friends: [33, 14, 3, 43, 35],
   },
   {
-    "id": 14,
-    "name": "Gloria Frami",
-    "address": "326 Littel Run, Tracemouth HI 02826-6898",
-    "email": "Jadon.OConnell@hotmail.com",
-    "strideLength": 3.5,
-    "dailyStepGoal": 12000,
-    "friends": [
-      34,
-      22
-    ]
+    id: 14,
+    name: 'Gloria Frami',
+    address: '326 Littel Run, Tracemouth HI 02826-6898',
+    email: 'Jadon.OConnell@hotmail.com',
+    strideLength: 3.5,
+    dailyStepGoal: 12000,
+    friends: [34, 22],
   },
   {
-    "id": 15,
-    "name": "Ezequiel Feest",
-    "address": "78801 Lauryn Plain, Lake Elinor MN 27856-9054",
-    "email": "Anthony_Toy@hotmail.com",
-    "strideLength": 4.4,
-    "dailyStepGoal": 5000,
-    "friends": [
-      10,
-      23,
-      35,
-      20,
-      5
-    ]
+    id: 15,
+    name: 'Ezequiel Feest',
+    address: '78801 Lauryn Plain, Lake Elinor MN 27856-9054',
+    email: 'Anthony_Toy@hotmail.com',
+    strideLength: 4.4,
+    dailyStepGoal: 5000,
+    friends: [10, 23, 35, 20, 5],
   },
   {
-    "id": 16,
-    "name": "Garnett Cruickshank",
-    "address": "992 Zita Mall, North Tremainemouth MA 19312-3532",
-    "email": "Laverna47@hotmail.com",
-    "strideLength": 3.9,
-    "dailyStepGoal": 10000,
-    "friends": [
-      25,
-      31,
-      3,
-      16
-    ]
+    id: 16,
+    name: 'Garnett Cruickshank',
+    address: '992 Zita Mall, North Tremainemouth MA 19312-3532',
+    email: 'Laverna47@hotmail.com',
+    strideLength: 3.9,
+    dailyStepGoal: 10000,
+    friends: [25, 31, 3, 16],
   },
   {
-    "id": 17,
-    "name": "Jade Walter",
-    "address": "796 Rogahn Track, Lubowitzhaven NJ 62669-3244",
-    "email": "Dylan_Langworth65@hotmail.com",
-    "strideLength": 4.3,
-    "dailyStepGoal": 5000,
-    "friends": [
-      8,
-      31,
-      40
-    ]
-  }
+    id: 17,
+    name: 'Jade Walter',
+    address: '796 Rogahn Track, Lubowitzhaven NJ 62669-3244',
+    email: 'Dylan_Langworth65@hotmail.com',
+    strideLength: 4.3,
+    dailyStepGoal: 5000,
+    friends: [8, 31, 40],
+  },
 ];
 
 export default userData;

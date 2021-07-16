f52dce17b62612b65f26121d87005306b73f420c
Justin Anthony <justincanthony@gmail.com>
2021-07-16 14:55:40 -0600

    Edit format, remove console log

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,16 +1,19 @@
-// methods need to get average water from all time
 class Hydration {
   constructor(userHydration) {
     this.userHydration = userHydration;
   }
-  // this needs to be adjusted for this class (it was moved from repo)
-  // getAvgOuncesAllTime(id) {
-  //   const userDays = this.data.filter((user) => user.userID === id);
-  //   const totalUserOunces = userDays.reduce((totalOunces, user) => {
-  //     return (totalOunces += user.numOunces);
-  //   }, 0);
-  //   return totalUserOunces / userDays.length;
-  // }
-}
 
+  getUserAvgOzAllTime() {
+    return Math.round(
+      this.userHydration.reduce((totalOunces, userEntry) => {
+        return (totalOunces += userEntry.numOunces);
+      }, 0) / this.userHydration.length
+    );
+  }
+
+  getUserOzByDate(date) {
+    return this.userHydration.find((userEntry) => userEntry.date === date)
+      .numOunces;
+  }
+}
 export default Hydration;

5f38df3dcf88b584a35533b9431445f6bd2ef229
Justin Anthony <justincanthony@gmail.com>
2021-07-16 13:12:11 -0600

    Create hydration class and properties

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,10 +1,16 @@
 // methods need to get average water from all time
-class hydration {
-  constructor(hydrationData) {
-    this.id = hydrationData.userID;
-    this.date = hydrationData.date;
-    this.numOunces = hydrationData.numOunces;
+class Hydration {
+  constructor(userHydration) {
+    this.userHydration = userHydration;
   }
+  // this needs to be adjusted for this class (it was moved from repo)
+  // getAvgOuncesAllTime(id) {
+  //   const userDays = this.data.filter((user) => user.userID === id);
+  //   const totalUserOunces = userDays.reduce((totalOunces, user) => {
+  //     return (totalOunces += user.numOunces);
+  //   }, 0);
+  //   return totalUserOunces / userDays.length;
+  // }
 }
 
 export default Hydration;

3e8639a03ca1760b53c1856ac589e28d5122cc4d
William Phelps <williamphelps13@gmail.com>
2021-07-16 21:47:06 -0700

    Add tests to test hydration class

--- src/data/hydration-test-data.js
+++ src/data/hydration-test-data.js
@@ -34,6 +34,26 @@ const hydrationData = [
     date: '2019/06/17',
     numOunces: 95,
   },
+  {
+    userID: 1,
+    date: '2019/06/18',
+    numOunces: 61,
+  },
+  {
+    userID: 1,
+    date: '2019/06/19',
+    numOunces: 91,
+  },
+  {
+    userID: 1,
+    date: '2019/06/20',
+    numOunces: 50,
+  },
+  {
+    userID: 1,
+    date: '2019/06/21',
+    numOunces: 50,
+  }
 ];
 
 export default hydrationData;

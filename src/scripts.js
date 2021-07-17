c00d498efe9d9a98b8aed8264f454dbd38f94ac7
Justin Anthony <justincanthony@gmail.com>
2021-07-16 18:34:24 -0600

    Fetch hydration data and match it the current user

--- src/scripts.js
+++ src/scripts.js
@@ -10,9 +10,10 @@ console.log('This is the JavaScript entry file - your code begins here.');
 import { getData } from './apiCalls';
 import UserRepository from './UserRepository';
 import User from './User';
+import HydrationRepo from './HydrationRepo';
 import Hydration from './Hydration';
 
-let userRepository, user;
+let userRepository, user, hydrationRepo, hydration, userIndex;
 // function to get data from the API
 
 const populateDataOnPage = () => {
@@ -33,9 +34,20 @@ const populateDataOnPage = () => {
   document.getElementById('friends').innerText = friends;
 };
 
+const getRandomIndex = () => Math.floor(Math.random() * 50);
+
+userIndex = getRandomIndex();
+
 getData('users')
   .then((data) => {
     userRepository = new UserRepository(data.userData);
-    user = new User(userRepository.data[0]);
+    user = new User(userRepository.data[userIndex]);
+    console.log(user);
   })
   .then(populateDataOnPage);
+
+getData('hydration').then((data) => {
+  hydrationRepo = new HydrationRepo(data.hydrationData);
+  hydration = new Hydration(hydrationRepo.getUserHydration(userIndex + 1));
+  console.log(hydration);
+});

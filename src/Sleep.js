67969ff71ccafea68761b1e7e4f65c9a3d3f04cf
Justin Anthony <justincanthony@gmail.com>
2021-07-18 14:21:46 -0600

    Create function to retieve a week of data

--- src/Sleep.js
+++ src/Sleep.js
@@ -4,33 +4,49 @@ class Sleep {
   }
 
   getUserAvgHrs() {
-    return Math.round(
-      this.userSleep.reduce((totalHrs, userEntry) => {
-        return (totalHrs += userEntry.hoursSlept);
-      }, 0) / this.userSleep.length * 10) / 10;
+    return (
+      Math.round(
+        (this.userSleep.reduce((totalHrs, userEntry) => {
+          return (totalHrs += userEntry.hoursSlept);
+        }, 0) /
+          this.userSleep.length) *
+          10
+      ) / 10
+    );
   }
 
   getUserAvgQuality() {
-    return Math.round(
-      this.userSleep.reduce((totalQuality, userEntry) => {
-        return (totalQuality += userEntry.sleepQuality);
-      }, 0) / this.userSleep.length * 10) / 10;
+    return (
+      Math.round(
+        (this.userSleep.reduce((totalQuality, userEntry) => {
+          return (totalQuality += userEntry.sleepQuality);
+        }, 0) /
+          this.userSleep.length) *
+          10
+      ) / 10
+    );
   }
 
   getUserHrsByDate(date) {
-    return this.userSleep.find(userEntry => userEntry.date === date).hoursSlept;
+    return this.userSleep.find((userEntry) => userEntry.date === date)
+      .hoursSlept;
   }
 
   getUserQualityByDate(date) {
-    return this.userSleep.find(userEntry => userEntry.date === date).sleepQuality;
+    return this.userSleep.find((userEntry) => userEntry.date === date)
+      .sleepQuality;
   }
 
-  // getUserHrsByWeek() {
-  
+  getUserHrsByWeek(date) {
+    const target = this.userSleep.find((userEntry) => userEntry.date === date);
+    const index = this.userSleep.indexOf(target);
+    if (index > -1) {
+      return this.userSleep.splice(index, 7);
+    }
+    return this.userSleep.slice(index, 7);
+  }
   // }
-
   // getUserQualityByWeek() {
-
   // }
 }
 

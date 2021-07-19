d998a2cb0998c6d3198cf757a6410f7c7618858f
Justin Anthony <justincanthony@gmail.com>
2021-07-18 21:31:49 -0600

    Create function to return avg sleep quality for all users

--- src/SleepRepo.js
+++ src/SleepRepo.js
@@ -2,14 +2,18 @@ class SleepRepo {
   constructor(data) {
     this.data = data;
   }
-  
-  getUserSleepData(id) {
-    return this.data.filter(userEntry => userEntry.userID === id);
-  }
-  
-  // getAvgQualityAllUsers() {
 
-  // }
+  getUserSleepData(id) {
+    return this.data.filter((userEntry) => userEntry.userID === id);
+  }
+
+  getAvgQualityAllUsers() {
+    return Math.round(
+      this.data.reduce((totalQuality, userQuality) => {
+        return (totalQuality += userQuality.sleepQuality);
+      }, 0) / this.data.length
+    );
+  }
 }
 
 export default SleepRepo;
